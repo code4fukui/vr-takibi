6506beeaab59b29d73e5a3a1030519406b661f69
Taisuke Fukuno <fukuno@jig.jp>
2024-06-06 08:03:21 +0900

    separate getClockPos

---
+++ getClockPos.js
@@ -0,0 +1,14 @@
+export const getClockPos = () => {
+	const d = new Date();
+	let n = d.getHours();
+	let m = d.getMinutes();
+	let l = d.getSeconds();
+	const ms = d.getMilliseconds();
+	l += ms / 1000;
+	m += l / 60;
+	n += m / 60;
+	const hour = n / 24 * Math.PI * 2 + Math.PI;
+	const min = m / 60 * Math.PI * 2 + Math.PI;
+	const sec = l / 60 * Math.PI * 2 + Math.PI;
+	return { hour, min, sec };
+};
